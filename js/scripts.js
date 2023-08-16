712e647b110ccc870eefc4216c7a8974d22a670a
vicmpg <vicmpg22@gmail.com>
2023-08-16 15:29:24 -0400

    Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-let favoriteFood= ('Pizza');
-document.write(favoriteFood);
+let pokemonList= [];
+pokemonList = [
+    {
+        name: 'Charmeleon',
+        height: 0.7,
+        type: ['Fire','Steel']
+    },
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        type: ['Water','Ice']
+    },
+    {
+        name: 'Machop',
+        height: 0.8,
+        type: ['Rock']
+    }
+]
