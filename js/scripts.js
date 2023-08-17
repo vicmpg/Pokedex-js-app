045b17d018a1e9589961d8e223070d7d93785acd
vicmpg <vicmpg22@gmail.com>
2023-08-17 15:01:18 -0400

    Added loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-let pokemonList= [];
-pokemonList = [
+let pokemonList= [
     {
-        name: 'Charmeleon',
-        height: 0.7,
+        name: 'Charmeleon', 
+        height: 0.7, 
         type: ['Fire','Steel']
     },
     {
-        name: 'Blastoise',
-        height: 1.6,
+        name: 'Blastoise', 
+        height: 1.6, 
         type: ['Water','Ice']
     },
     {
-        name: 'Machop',
-        height: 0.8,
+        name: 'Machop', 
+        height: 0.8, 
         type: ['Rock']
     }
-]
+];
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height <1){
+        document.write("<p>"+ pokemonList[i].name + pokemonList[i].height +"</p>"+ ' Is a small pokemon');
+    }else {
+        document.write("<p>"+ pokemonList[i].name + pokemonList[i].height +"</p>"+  ' Wow, that is big!');
+    }
+}
