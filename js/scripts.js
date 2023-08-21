837a209130efc20125c0c6e2ce123d4737e52f0a
vicmpg <vicmpg22@gmail.com>
2023-08-21 10:45:10 -0400

    Used a for each function instead of the for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
-let pokemonList= [
-    { name: 'Charmeleon', height: 0.7, type: ['Fire','Steel'] },
-    { name: 'Blastoise', height: 1.6, type: ['Water','Ice'] },
-    { name: 'Machop', height: 0.8, type: ['Rock'] }
+let pokemonList = [
+        { 
+            name: 'Charmeleon',
+            height: 0.7, 
+            type: ['Fire','Steel'] 
+        },
+        { 
+            name: 'Blastoise', 
+            height: 1.6, 
+            type: ['Water','Ice'] 
+        },
+        { 
+            name: 'Machop', 
+            height: 0.8, 
+            type: ['Rock'] 
+        }
 ];
 
-for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <1){
-        document.write("<p>"+ pokemonList[i].name + pokemonList[i].height +"</p>"+ ' Is a small pokemon');
-    }else {
-        document.write("<p>"+ pokemonList[i].name + pokemonList[i].height +"</p>"+  ' Wow, that is big!');
-    }
-}
+pokemonList.forEach( item => console.log(item) );
+
+
+
+  
