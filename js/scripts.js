129640109170fa0f7b58247ab1136585d77c3f86
vicmpg <vicmpg22@gmail.com>
2023-08-26 10:01:04 -0400

    Task 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  
+    let modalContainer = document.querySelector('#modal-container');
     function add(pokemon) {
       if (
         typeof pokemon === "object" &&
@@ -64,6 +64,54 @@ let pokemonRepository = (function () {
         console.log(item);
       });
     }
+    function showModal(title, text) {
+      modalContainer.innerHTML = '';
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+  
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+  
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+  
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+  
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+      
+      modalContainer.classList.add('is-visible');
+    }
+  
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+    }
+  
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+    
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  
+    document.querySelector('#show-modal').addEventListener('click', () => {
+      showModal('Modal title', 'This is the modal content!');
+    });
+  
+    // THE RETURN STATEMENT HERE
   
     return {
       add: add,
@@ -71,7 +119,9 @@ let pokemonRepository = (function () {
       addListItem: addListItem,
       loadList: loadList,
       loadDetails: loadDetails,
-      showDetails: showDetails
+      showDetails: showDetails,
+      showModal: showModal,
+      hideModal: hideModal
     };
   })();
   
